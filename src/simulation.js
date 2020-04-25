54ac8364c7fa91b6f2417dd82878013a6b147013
clohne <christoph+github-clohne@lohner.nl>
2020-04-25 10:18:23 +0200

    #1 Introduced scenario for configuration

--- src/simulation.js
+++ src/simulation.js
@@ -3,18 +3,41 @@ const $quarantine = document.getElementById("quarantine");
 const $run = document.getElementById("run");
 
 $run.addEventListener("click", () => {
-  const q = parseFloat($quarantine.value);
-  const pop = new Population(1000, q, 3);
-  const graph = new Graph(pop);
-  graph.context.clearRect(0, 0, graph.width, graph.height);
-  $overlay.classList.remove("active");
+    const scenario = new Scenario();
+    scenario.configure(1);
+    const pop = new Population(scenario.population.size, scenario.behaviour.quarantineRate, scenario.population.patientZeroes);
+    const graph = new Graph(pop);
+    graph.context.clearRect(0, 0, graph.width, graph.height);
+    $overlay.classList.remove("active");
 
-  function run() {
-    pop.tick();
-    graph.tick();
-    if (graph.done) $overlay.classList.add("active");else
-    requestAnimationFrame(run);
-  }
+    function run() {
+        pop.tick();
+        graph.tick();
+        if (graph.done) $overlay.classList.add("active");else
+        requestAnimationFrame(run);
+    }
 
-  run();
+    run();
 });
+
+class Scenario {
+    constructor(){
+        this.population.size = 1000;
+        this.population.patientZeroes = 3;
+        this.behaviour.quarantineRate = 0.5;
+        this.behaviour.socialDistanceRate = 0.75;
+        this.behaviour.socialDistanceDiscipline = 0.6;
+        this.virus.incubationTime = 4;
+        this.virus.symptomLagTime = 8;
+        this.virus.RecoveryTime = 16;
+    }
+
+    configure(id){
+        switch(id) {
+            case 1:
+                this.behaviour.socialDistanceRate = parseFloat($quarantine.value);
+              break;
+            default:
+          } 
+    }
+}
