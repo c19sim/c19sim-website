2fc3ece21f57424b3f6a89534b9e38cdc38166f7
Levi Waddingham <Levi.Waddingham@tessella.com>
2020-04-25 17:40:25 +0200

    #23: Added code to allow multiple scrolly elements and message passing down to iframes.

--- src/simulation/simulation.js
+++ src/simulation/simulation.js
@@ -2,9 +2,40 @@ const $overlay = document.getElementById("overlay");
 const $quarantine = document.getElementById("quarantine");
 const $run = document.getElementById("run");
 
+/*document.body.addEventListener('iframe-out-of-screen', () => {
+    console.log('Caught the event!');
+});*/
+
+function bindEvent(element, eventName, eventHandler) {
+    if (element.addEventListener) {
+        element.addEventListener(eventName, eventHandler, false);
+    } else if (element.attachEvent) {
+        element.attachEvent('on' + eventName, eventHandler);
+    }
+}
+
+// Listen to messages from parent window
+bindEvent(window, 'message', function (e) {
+    console.log('Caught the event!', e);
+    // TODO: pause all simulations
+    results.innerHTML = e.data;
+});
+
+function getScenarioId() {
+    var params = location.href.split('?')[1].split('&');
+    console.log('params', params);
+    data = {};
+    for (x in params)
+    {
+        data[params[x].split('=')[0]] = params[x].split('=')[1];
+    }
+    return data.key;
+}
+
 $run.addEventListener("click", () => {
+    var scenarioId = getScenarioId();
     const scenario = new Scenario();
-    scenario.configure(1);
+    scenario.configure(scenarioId);
     const pop = new Population(scenario.population.size, scenario.behaviour.quarantineRate, scenario.population.patientZeroes);
     const graph = new Graph(pop);
     graph.context.clearRect(0, 0, graph.width, graph.height);
@@ -36,11 +67,12 @@ class Scenario {
             symptomLagTime: 8,
             RecoveryTime: 16
         };
+        
     }
 
     configure(id) {
         switch (id) {
-            case 1:
+            case 'scenario1':
                 this.behaviour.socialDistanceRate = parseFloat($quarantine.value);
                 break;
             default:
